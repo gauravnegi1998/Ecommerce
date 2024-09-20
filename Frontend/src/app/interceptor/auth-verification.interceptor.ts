521020d8de5110488c89ae5424df64645736de6c
gauravnegi101 <gauravnegi@csgroupchd.com>
2024-09-20 12:18:29 +0530

    only backend now

--- Frontend/src/app/interceptor/auth-verification.interceptor.ts
+++
@@ -1,5 +0,0 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-
-export const authVerificationInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
-};
--- Frontend/src/environments/dev.environment.ts
+++
@@ -1,7 +0,0 @@
-export const devEnvironment = {
-    production: false,
-    // apiUrl: "http://localhost:8080",
-    apiUrl: "https://ecommerce-backend-tan-phi.vercel.app",
-}
-
-//  --host 0.0.0.0
